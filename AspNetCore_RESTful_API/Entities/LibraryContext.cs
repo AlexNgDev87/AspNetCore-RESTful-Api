f1987dbe7cf508733361aaf9fd44f685d1d635f3
AlexNg <alex-ng-dev@hotmail.com>
2018-12-10 16:33:11 +1100

    initial commit

---
+++ AspNetCore_RESTful_API/Entities/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApi.Entities
+{
+    public class LibraryContext: DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=library.db");
+        }
+    }
+}
---
+++ AspNetCore_RESTful_API/Services/LibraryRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApi.Entities;
+
+namespace LibraryApi.Services
+{
+    public class LibraryRepository : IRepository
+    {
+        private LibraryContext _context;
+
+        public LibraryRepository(LibraryContext context)
+        {
+            _context = context;
+        }
+
+
+        public void AddAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBookForAuthor(Guid authorId, Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AuthorExsts(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author GetAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book GetBookForAuthor(Guid authorId, Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Book> GetBooksForAuthor(Guid authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAuthor(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBookForAuthor(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
